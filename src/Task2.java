15404d84963674c1c8772dfd65a27f0495457f46
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-11-29 09:43:32 -0500

    nn

--- src/Task2.java
+++ src/Task2.java
@@ -1,5 +1,9 @@
-public class Main {
+import java.util.Scanner;
+public class Task2 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+    int favInt = InputHelper.getInt(scan,"Please enter your favorite Integer");
+    double favDouble = InputHelper.getDouble(scan,"Please enter your favorite Double");
+        System.out.println("Your favorite Integer is " + favInt + ", and your favorite Double is " + favDouble);
     }
 }
--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Task3 {
+import java.util.Scanner;
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int month = InputHelper.getRangedInt(scan, "Enter your birth month", 1, 12);
+        int year = InputHelper.getRangedInt(scan, "Enter your birth year", 1950, 2010);
+        int hour = InputHelper.getRangedInt(scan, "Enter your birth hour", 1, 24);
+        int minute = InputHelper.getRangedInt(scan, "Enter your birth minute", 1, 59);
+        int day;
+
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            day = InputHelper.getRangedInt(scan, "Enter your birth day", 1, 31);
+        } else if (month == 2) {
+            day = InputHelper.getRangedInt(scan, "Enter your birth day", 1, 28);
+        }
+        else {
+            day = InputHelper.getRangedInt(scan, "Enter your birth day", 1, 30);
+        }
+
+    }
 }
