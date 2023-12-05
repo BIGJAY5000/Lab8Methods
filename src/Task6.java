0b03c3346cf9fc49d2ba13534847ef35456a0903
BIGJAY5000 <144457717+BIGJAY5000@users.noreply.github.com>
2023-12-04 22:55:10 -0500

    Update Task6.java

--- src/Task6.java
+++ src/Task6.java
@@ -4,4 +4,34 @@ public class Task6 {
         Scanner scan = new Scanner(System.in);
 
     }
+     public static void prettyHeader(String msg){
+        String x = "";
+        int y = msg.length();
+        int z = (54 - y)/2;
+        int a = 0;
+        for (int i = 0; i <= 60; i++) {
+            x += "*";
+        }
+        System.out.println(x);
+        x = "";
+
+        for (int j = 0; j <=60; j++) {
+            if (j < 3 || j > 57) {
+                x += "*";
+            } else if (j > z+3 && j < 57-z) {
+                x += (msg.substring(a, a + 1));
+                a++;
+            }
+            else {
+                x += " ";
+            }
+        }
+        System.out.println(x);
+
+        x = "";
+        for (int k = 0; k <= 60; k++) {
+            x += "*";
+        }
+        System.out.println(x);
+    }
 }
